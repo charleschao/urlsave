8b6358c7329e68ec1713343807a6c4487fde8703
Charles Chao <charleschao@gmail.com>
2013-01-18 14:59:06 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Urlsave::Application.config.secret_token = 'bd3c137be372b33cfa3c3d8e06624748d5df4cd1488abaa2b9862ff039bd1fa0c3ee0f524878b5f11e502db4a0dfe59b12071722af9a2f8ad83175b1901f0a05'
